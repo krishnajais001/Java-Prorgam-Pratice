342790cef4e2dbdb8be8b37362be9841c684ec74
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-03-15 23:48:59 +0530

    Add files via upload

---
+++ Ques1.java
@@ -0,0 +1,11 @@
+ /* Write a Java program to print 'Hello' on screen and your name on a separate line.
+Expected Output :
+Hello
+Alexandra Abramov */
+import java.lang.*;
+ class Ques1{
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Krishna Kumar");
+    }
+}
