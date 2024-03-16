0ad916626d85458bb96dd4bf74d5e5b3d56dfa2f
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-03-16 13:18:49 +0530

    Add files via upload

---
+++ typeCasting.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+
+class typeCasting{
+	public static void main(String args[]){
+		{
+			int a=10;
+			double b=a;
+			System.out.println(" Implicit Type Conversion:"+b);
+		}
+		
+			double a=10.1244;
+			int b=(int)a;
+			System.out.println(" Explicit Type Conversion:"+b);
+	}
+}
