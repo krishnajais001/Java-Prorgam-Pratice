839a404db16083394de66955baff0e049220e090
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-03-16 11:36:36 +0530

    Add files via upload

---
+++ checkSizeAndRange.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+
+class checkSizeAndRange{
+	public static void main(String atgs[]){
+			// BYTE
+		System.out.println("Byte MIN:"+Byte.MIN_VALUE);
+		System.out.println("Byte MAX:"+Byte.MAX_VALUE);
+		System.out.println("Byte Bytes:"+Byte.BYTES);
+		
+		// SHORT
+		System.out.println("Short	MIN:"+Short.MIN_VALUE);
+		System.out.println("Short MAX:"+Short.MAX_VALUE);
+		System.out.println("Short Byte:"+Short.BYTES);
+		
+		
+		
+		// INTEGER
+		System.out.println("INT MIN:"+Integer.MIN_VALUE);
+		System.out.println("INT MAX:"+Integer.MAX_VALUE);
+		System.out.println("INT BYTE:"+Integer.BYTES);
+		
+	
+	}
+}
---
+++ simpleIntrest.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.Scanner;
+class simpleIntrest
+{
+public static void main(String args[])
+{
+String n;
+Scanner val=new Scanner(System.in);
+System.out.print("Name :");
+n=val.next();
+
+}
+}
