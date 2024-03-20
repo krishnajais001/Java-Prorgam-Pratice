fe71f9147be0d8f49b0577c7aa10bc33f84ba4d0
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-03-20 22:44:09 +0530

    Add files via upload

---
+++ Ques16.java
@@ -0,0 +1,23 @@
+/* 16. Write a Java program to print a face.
+Expected Output
+
+ +"""""+                                                 
+[| o o |]                                                
+ |  ^  |                                                 
+ | '-' |                                                 
+ +-----+
+*/
+
+import java.lang.*;
+import java.util.*;
+
+class Ques16{
+	public static void main(String args[]){
+		System.out.println(" +\"\"\"\"\"+ ");
+		System.out.println("[| o o |]");
+		System.out.println(" |  ^  |");
+		System.out.println(" | '-' |");
+		System.out.println(" +-----+");
+
+	}
+}
