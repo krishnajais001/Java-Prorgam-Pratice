e8ef4369258ee971f2c0fda3bd3d173122d1462d
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-03-22 21:15:33 +0530

    Add files via upload

---
+++ incDec.java
@@ -0,0 +1,20 @@
+
+import java.lang.*;
+
+class incDec{
+	public static void main(String args[]){
+		
+		int i=5;
+		i++;
+		System.out.println("Integer:"+i);
+		float f=3.5f;
+		f++;
+		System.out.println("Float:"+f);
+		char c='A';
+		c++;
+		System.out.println("Char:"+c);
+		byte by=7;
+		by++;
+		System.out.println("Byte:"+by);
+	}
+}
