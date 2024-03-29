9b32e0fe3de0d62ebc14230dec69946b238c22cd
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-03-29 19:13:44 +0530

    Add files via upload

---
+++ creatingStringObject.java
@@ -0,0 +1,34 @@
+import java.lang.*;
+class creatingStringObject {
+
+    public static void main(String[] args) {
+
+    // String str1="Java Program";
+    // System.out.println(str1);
+
+    // String str2 =new String("JAVA");
+    // System.out.println(str2);
+
+    // char c[]={'H','E','L','L','O'};
+    // String str3= new String(c);
+    // System.err.println(c);
+   
+    // byte b[]={65,66,67,68};
+    // String str4=new String(b);
+    // System.out.println(str4);
+
+        // String str1="Java"; // Java is maintaining a single object in the pool
+        // String str2="Java";
+        // System.out.println(str1==str2);
+
+
+        // String str1="Java"; / Java is maintaining a single object in the pool
+        // String str2="java";
+        // System.out.println(str1==str2);
+
+        //  String str1="Java"; // Java is maintaining a single object in the pool
+        //  String str2=new String("Java"); // new object will be created in a heap
+        //  System.out.println(str1==str2);
+
+    }
+}
