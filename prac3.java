46f17c20840a49938577d5e960e1ab740599a920
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-05-05 23:04:53 +0530

    Add files via upload

---
+++ prac3.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.*;
+class prac3{
+    public static void main (String args[]){
+        String name;
+        Scanner val=new Scanner(System.in);
+        System.out.println("Enter Your Name :");
+        name=val.nextLine();
+        System.out.println("My Name is "+name);
+    }
+}
---
+++ prac4.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.*;
+class prac4{
+    public static void main (String args[]){
+       boolean isFolow;
+       Scanner val=new Scanner (System.in);
+       System.out.println("Enter value");
+       isFolow=val.nextBoolean();
+       System.out.println(isFolow);
+    }
+}
