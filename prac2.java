c6576303f15780a8ace253d3ad6fe073fb1ac55f
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-05-05 22:54:58 +0530

    Add files via upload

---
+++ prac2.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+class prac2{
+    public static void main(String args[]){
+        int a,b,c;
+        Scanner val=new Scanner(System.in);
+        System.out.println("Enter Value :");
+        a=val.nextInt();
+        b=val.nextInt();
+        c=a+b;
+        System.out.println("Sum is :"+c);
+    }
+}
