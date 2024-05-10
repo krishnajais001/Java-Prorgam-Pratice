a6fa4fe0fe9f804a9c7da7bb87c4b1b651b31014
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-05-10 06:13:09 +0530

    Char Scanner

---
+++ prac9.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.*;
+public class prac9{
+    public static void main(String[] args) {
+        char branch;
+        Scanner val=new Scanner(System.in);
+        System.out.println("Enter Branch :");
+        branch=val.next().charAt(0);
+        System.out.println("Branch is : "+branch);
+    }
+}
