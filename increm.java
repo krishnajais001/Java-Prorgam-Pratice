d4f274ec84fea9303ac0b920035ef7f80013a8b6
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-02 21:45:51 +0530

    Add files via upload

---
+++ increm.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+public class increm {
+  public static void main(String[] args) {
+    int x=5,y;
+    // y=x++;
+    // System.out.println(" Post Increment : " + x++);
+    // System.out.println(" Original Value : "+x);
+    // System.out.println(y);
+    System.out.println(" Pre Increment : "+ ++x);
+    System.out.println(" Original Value : "+x);
+  }  
+}
