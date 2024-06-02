51ce4ee767f3948adbac357f9ca21a09457a9bec
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-02 22:48:47 +0530

    Add files via upload

---
+++ typeconver.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.lang.*;
+public class typeconver {
+    public static void main(String[] args) {
+       
+
+// int y=5;
+// float z=y;
+// System.out.println(z);
+float x=5.4f;
+int y=(int)x;
+System.out.println(y);
+    }
+}
