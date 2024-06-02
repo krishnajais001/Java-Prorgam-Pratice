730e6bca2895d6fca2d9f28dcd5c817332067077
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-02 21:49:35 +0530

    Add files via upload

---
+++ decrem.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+public class decrem {
+  public static void main(String[] args) {
+    int x=5,y;
+    // y=x++;
+    System.out.println(" Post Decrement : " + x--);
+    System.out.println(" Original Value : "+x);
+
+    // System.out.println(y);
+    // System.out.println(" Pre Decrement : "+ --x);
+    // System.out.println(" Original Value : "+x);
+  }  
+}
