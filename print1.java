ab4d6845c96406752d30955cf34c47781d23117b
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-05 23:06:12 +0530

    Add files via upload

---
+++ print1.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+
+public class print1 {
+    public static void main(String[] args) {
+        int x=10,y=20;
+        System.out.println(x+y +" Sum ");
+        System.out.println(" sum "+(x+y));
+    }
+}
