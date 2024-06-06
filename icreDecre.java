8a45fb5788ff478a896130236e8a208f2aeb9a99
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-06 16:01:47 +0530

    Add files via upload

---
+++ icreDecre.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+public class icreDecre {
+   public static void main(String[] args) {
+    // int x=5;
+    // float y=x;
+    // System.out.println("X : "+x);
+    // System.out.println("Y : "+y);
+double x=5.6;
+int y=(int)x;
+System.out.println("X : "+x);
+System.out.println("Y : "+y);
+}
+}
