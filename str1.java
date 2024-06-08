2165a5bc5a7848419e95e299558be50de01903d1
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-08 11:02:13 +0530

    Add files via upload

---
+++ str1.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.*;
+
+public class str1{
+    public static void main(String[] args) {
+        // String str2="Java"; -----> it is created in pool space memory 
+        // System.out.println(str2);
+
+        // String str3=new String("Java"); -----> it is created in heap space memory due to the use of new keyword
+        // System.out.println(str3);
+      
+        // char c[]={'H','E','L','L','O'};
+        // System.out.println(c);
+        // String str4=new String(c);
+        // System.out.println(str4);
+      
+    }
+}
