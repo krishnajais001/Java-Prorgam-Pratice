58eb5131f5ed61d321a51c84ec8fee099a3e1ba2
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-08 11:59:55 +0530

    Add files via upload

---
+++ stringMethod1.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+public class stringMethod1 {
+    public static void main(String[] args) {
+        String str1=new String ("Java");
+        System.out.println("Length of Java is "+str1.length());
+        String name=new String("Krishna");
+        System.out.println("Length of Krishna is "+name.length());
+        String language="Java";
+    System.out.println("Length of language is "+language.length());    }
+}
