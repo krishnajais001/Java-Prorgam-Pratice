2a1bed481827ebbb21ad7c77e037b7f735091b54
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-11 15:59:05 +0530

    Add files via upload

---
+++ replace2.java
@@ -0,0 +1,7 @@
+public class replace2 {
+    public static void main(String[] args) {
+      String str="elephant";
+      String str1=str.replace("e", "p");
+      System.out.println(str1);
+    }
+}
