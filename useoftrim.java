ef961dab02ea032feb7d98b3c3efa1c2d45dd538
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-11 15:49:30 +0530

    Add files via upload

---
+++ useoftrim.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.*;
+public class useoftrim {
+  public static void main(String[] args) {
+    String str="   krishna ";
+    String str1=str.trim();
+    System.out.println("Before Trim :"+str);
+   System.out.print("After Trim :"+str1);
+  }
+    
+}
