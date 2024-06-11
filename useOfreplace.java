1657d5f118ff86f516bfe49085f5a0071d6284bd
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-11 15:55:58 +0530

    Add files via upload

---
+++ useOfreplace.java
@@ -0,0 +1,7 @@
+public class useOfreplace {
+    public static void main(String[] args) {
+        String str="krishnajais7091@gmail.com";
+        String str1=str.replace("krishnajais","Ashish");
+        System.out.println("String After Conversion :"+str1);
+    }
+}
