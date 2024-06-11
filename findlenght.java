6baafcf2580d32c4725c64800a51083a02887088
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-11 11:45:44 +0530

    Add files via upload

---
+++ findlenght.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.*;
+
+public class findlenght{
+    public static void main(String[] args) {
+        // Find Length of a string
+        String str; // str is a variable name
+        Scanner val=new Scanner(System.in);
+        System.out.print("Enter String type Value :");
+        str=val.next();
+        int Len=str.length(); // it retuns a integer type value
+        System.out.println("Length of " + str + " is " +Len);
+      // System.out.println("Lenghth of String is : "+Len);
+       
+
+    }
+}
