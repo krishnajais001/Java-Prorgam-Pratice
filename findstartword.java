7684c3e9536d2f414a9854f4021038c1a10520bd
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-12 10:38:41 +0530

    Add files via upload

---
+++ findstartword.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+public class findstartword {
+public static void main(String[] args) {
+    String str="www.google.com";
+    boolean str1=str.startsWith("www");
+    boolean str2=str.endsWith("com");
+    System.out.println(str1);
+    System.out.println(str2);
+}
+
+    
+}
