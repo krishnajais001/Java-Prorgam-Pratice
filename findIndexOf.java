79a8dcdb3d7706049dfc2dc19995815a58d652e0
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-12 10:50:13 +0530

    Add files via upload

---
+++ findIndexOf.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+
+public class findIndexOf {
+    public static void main(String[] args) {
+        String str="www.google.co.in";
+        int str2=str.indexOf("g");
+        System.out.println(str2);
+    }
+}
