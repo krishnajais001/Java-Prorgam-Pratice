2bd06d810d6ea4ca74d2a62421c528524194ce1c
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-12 10:43:39 +0530

    Add files via upload

---
+++ findcharAt.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+import java.util.*;
+public class findcharAt {
+    public static void main(String[] args) {
+        String str="www.google.co.in";
+        char str1=str.charAt(3);
+        System.out.println(str1);
+    }
+}
