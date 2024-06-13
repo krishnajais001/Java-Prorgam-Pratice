b4adcc234be70c61328921ed646c2e245e8d5ef8
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-13 15:40:25 +0530

    Add files via upload

---
+++ equal.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class equal {
+    public static void main(String[] args) {
+        String str1="Java";
+        String str2=new String("Python");
+        // System.out.println(str1.equals(str2));
+        // System.out.println(str2==str1);
+        
+        System.out.println( str1.compareTo(str2));
+    }
+    
+}
