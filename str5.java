0676c6cd65c6030a81a9adb1c994bdebbec0e502
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-15 11:34:48 +0530

    Add files via upload

---
+++ str5.java
@@ -0,0 +1,8 @@
+
+public class str5 {
+    public static void main(String[] args) {
+        String str="elephent";
+        String str6=str.replace("elephent", "krishna");
+        System.out.println(str6);
+    }
+}
