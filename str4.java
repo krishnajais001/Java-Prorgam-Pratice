cd31aa36b5197d4d6f80165939251a70f39ebbc6
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-15 11:34:33 +0530

    Add files via upload

---
+++ str4.java
@@ -0,0 +1,7 @@
+public class str4 {
+    public static void main(String[] args) {
+        String str="Elephant";
+        String str1=str.substring(0,3);
+        System.out.println(str1);
+    }
+}
