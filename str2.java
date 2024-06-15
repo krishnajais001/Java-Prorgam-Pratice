1a9820002189f150e170c60f608b344d1732e781
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-15 11:20:13 +0530

    Add files via upload

---
+++ str2.java
@@ -0,0 +1,9 @@
+public class str2 {
+    public static void main(String[] args) {
+        String str2="my name is krishna";
+        String str3=str2.toUpperCase();
+        System.out.println(str3);
+        String str4=str3.toLowerCase();
+        System.out.println(str4);
+    }
+}
