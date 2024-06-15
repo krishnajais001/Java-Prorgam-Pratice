1e5a7fe6e8eb31394af0cb9d65383b9f7db55297
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-15 11:20:33 +0530

    Add files via upload

---
+++ str3.java
@@ -0,0 +1,8 @@
+public class str3 {
+    public static void main(String[] args) {
+        String str="   My name is krishna";
+        String str1=str.trim();
+        System.out.println(str);
+        System.out.println(str1);
+    }
+}
